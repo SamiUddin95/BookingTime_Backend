dbe0812bbf9087798c91a0955878ae3e3d0b9a1e
Danial Niaz <DanialNiaz234@gmail.com>
2025-05-04 14:11:05 +0500

    merge in main

--- BookingTime/DTO/RequestModel/PropertiesFilterRequestModel.cs
+++ BookingTime/DTO/RequestModel/PropertiesFilterRequestModel.cs
@@ -9,9 +9,21 @@
 
             public int? priceRangeFrom { get; set; }
             public int? priceRangeTo { get; set; }
+            public int? cityId { get; set; }
             public int? ratingId { get; set; }
+            public DateTime? CheckIn { get; set; }
+            public DateTime? CheckOut { get; set; }
             public List<hotelType> hotelTypes { get; set; }
             public List<amenity> amenities { get; set; }
+            public List<IdWrapper> BeachAccess { get; set; } = new();
+            public List<IdWrapper> EntirePlaces { get; set; } = new();
+            public List<IdWrapper> Facilities { get; set; } = new();
+            public List<IdWrapper> FunThingsToDo { get; set; } = new();
+            public List<IdWrapper> PopularFilter { get; set; } = new();
+            public List<IdWrapper> PropertyType { get; set; } = new();
+            public List<IdWrapper> PropertyAccessibility { get; set; } = new();
+            public List<IdWrapper> RoomAccessibility { get; set; } = new();
+            public List<IdWrapper> RoomFacilities { get; set; } = new();
         }
 
         public class hotelType
@@ -19,6 +31,9 @@
             public int? hotelTypeId { get; set; }
 
         }
+        public class IdWrapper
+        {
+            public int Id { get; set; }
+        }
     }
-    
 }
